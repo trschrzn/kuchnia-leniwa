70caa71ca087913fef1b48de8e97e3d74a4eea43
Teresa Chrzan <121943991+trschrzn@users.noreply.github.com>
2023-05-08 12:42:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ function scrollFunction() {
     scrollBtn.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
